ae7fbf2254d1cdc3723eae87196c5fb77f09ff9d
Berkay YILDIRIM <yildirim.berkay@outlook.com>
2025-04-23 19:41:05 +0300

    The pages folder has been added and tested with Git.
    The Pages folder has been added, and the Home component has been called inside the app.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
 import styles from './App.module.css';
+import Home from './pages/Home';
 
 export function App() {
   return (
-    <>
-      <header>
-        <h1>HubX Frontend Assignment</h1>
-      </header>
-
-      <main className={styles.main}></main>
-    </>
+    <Home />
   );
 }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+
+const Home = () => {
+    return (
+        <>
+            <header>
+                <h1>HubX Frontend Case - Berkay YILDIRIM</h1>
+            </header>
+        </>
+    );
+};
+
+export default Home;
