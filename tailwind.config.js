b64a4f31907bfe908f8208022550f84ed0aed602
Berkay YILDIRIM <berkay.yildirim@valprovia.com>
2025-04-24 23:43:35 +0300

    tailwind added to project

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Bütün component dosyalarını tarar
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
